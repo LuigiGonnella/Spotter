15a8c06bb6dda558495c02b885c9902a6100db82
LUIGI GONNELLA s341988 <s341988@studenti.polito.it>
2025-08-21 10:38:47 +0200

    update

--- backend/services/userService.mjs
+++ backend/services/userService.mjs
@@ -47,36 +47,4 @@ export async function updateUser(userData) { //posso passare solo i campi da mod
     }
 }
 
-export async function createRefreshToken(refreshData) {
 
-    const token = await prisma.refreshToken.create({
-        data: refreshData
-    });
-
-    return token;
-}
-
-export async function findTokenById(jti) {
-    try {
-        return await prisma.refreshToken.findUnique({
-            where: {jti}
-        })
-    }
-    catch(error) {
-        throw new Error(`Token not found`);
-    }
-}
-
-export async function updateToken(tokenData) {
-    try {
-        return await prisma.user.update(
-        {
-            where: {jti: tokenData.jti},
-            data: tokenData
-        }
-        )
-    }
-    catch(error) {
-        throw new Error(`Token not found`);
-    }
-}
